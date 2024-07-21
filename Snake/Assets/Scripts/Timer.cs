479ada643e6cea8429d070a693ce7d1d6df45d4c
Acree <tacree06@gmail.com>
2024-07-21 02:16:13 -0400

    changed borders and should be fine now

--- Snake/Assets/Scripts/Timer.cs
+++ Snake/Assets/Scripts/Timer.cs
@@ -2,20 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; // Include this for scene management
 
 public class Timer : MonoBehaviour
 {
-
-    //variable
-    public float TimeLeft = 10;
-
+    // Variable
+    public float TimeLeft = 15; // Starts at 15 seconds
     public Text TimerText;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Update the timer text initially to show the starting time
+        UpdateTimer(TimeLeft);
     }
 
     // Update is called once per frame
@@ -23,26 +22,26 @@ public class Timer : MonoBehaviour
     {
         if (TimeLeft > 0)
         {
-            TimeLeft += Time.deltaTime;
-
-            
+            TimeLeft -= Time.deltaTime;
         }
-
-       else
-       {
-            Debug.Log ("Game Over");
+        else
+        {
+            Debug.Log("Game Over");
             TimeLeft = 0;
-       }
-                updateTimer(TimeLeft);
+            // Load the Game Over scene
+            SceneManager.LoadScene("EndScene");
+        }
+        UpdateTimer(TimeLeft);
     }
 
-    void updateTimer(float currentTime)
+    void UpdateTimer(float currentTime)
     {
-        currentTime += 1;
+        // Ensure currentTime doesn't go below 0
+        if (currentTime < 0) currentTime = 0;
 
-        float minutes = Mathf.FloorToInt (currentTime / 60);
-        float seconds = Mathf.FloorToInt (currentTime % 60);
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
 
-        TimerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        TimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
